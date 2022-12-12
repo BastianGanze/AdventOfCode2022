edd89c91273d4c132a5752d2d3b27a0717855048
bastianganze <bganze@foxbase.de>
2022-12-12 08:13:21 +0100

    day 12

---
+++ src/bin/12/grid.rs
@@ -0,0 +1,160 @@
+use std::cmp::Ordering;
+
+pub type CostType = u32;
+pub type MaskType = bool;
+
+#[derive(Debug, Clone)]
+pub struct Grid {
+    pub fields: Vec<Vec<(CostType, MaskType)>>,
+    size: (usize, usize),
+}
+
+pub fn manhattan_distance(p1: (usize, usize), p2: (usize, usize)) -> u32 {
+    (p1.0.abs_diff(p2.0) + p1.1.abs_diff(p2.1)) as u32
+}
+
+impl Grid {
+    pub fn new(size: (usize, usize)) -> Grid {
+        Grid {
+            fields: vec![vec![(0, false); size.1]; size.0],
+            size: size.clone(),
+        }
+    }
+
+    pub fn get_size(&self) -> (usize, usize) {
+        self.size.clone()
+    }
+
+    pub(self) fn is_field_in_bounds(&self, y: i32, x: i32) -> bool {
+        y >= 0 && x >= 0 && y < self.size.0 as i32 && x < self.size.1 as i32
+    }
+
+    pub fn get_field_height(&self, y: usize, x: usize) -> CostType {
+        self.fields[y][x].0
+    }
+
+    pub fn set_field_height(&mut self, y: usize, x: usize, cost: CostType) {
+        if !self.is_field_in_bounds(y as i32, x as i32) {
+            return;
+        }
+
+        self.fields[y][x].0 = cost;
+    }
+
+    pub fn mark_field(&mut self, y: usize, x: usize) {
+        self.fields[y][x].1 = true;
+    }
+
+    pub fn is_field_marked(&self, y: usize, x: usize) -> bool {
+        self.fields[y][x].1
+    }
+
+    pub fn get_left(&self, y: usize, x: usize) -> Option<CostType> {
+        if !self.is_field_in_bounds(y as i32, x as i32 - 1) {
+            return None;
+        }
+
+        Some(self.get_field_height(y, x - 1))
+    }
+
+    pub fn get_right(&self, y: usize, x: usize) -> Option<CostType> {
+        if !self.is_field_in_bounds(y as i32, x as i32 + 1) {
+            return None;
+        }
+
+        Some(self.get_field_height(y, x + 1))
+    }
+
+    pub fn get_top(&self, y: usize, x: usize) -> Option<CostType> {
+        if !self.is_field_in_bounds(y as i32 - 1, x as i32) {
+            return None;
+        }
+
+        Some(self.get_field_height(y - 1, x))
+    }
+
+    pub fn get_bottom(&self, y: usize, x: usize) -> Option<CostType> {
+        if !self.is_field_in_bounds(y as i32 + 1, x as i32) {
+            return None;
+        }
+
+        Some(self.get_field_height(y + 1, x))
+    }
+
+    pub fn get_unmarked_neighbours(&self, y: usize, x: usize) -> Vec<(usize, usize, CostType)> {
+        let mut neighbours = Vec::new();
+        let (left_o, top_o, right_o, bottom_o) = (
+            self.get_left(y, x),
+            self.get_top(y, x),
+            self.get_right(y, x),
+            self.get_bottom(y, x),
+        );
+        if let Some(n) = left_o {
+            if !self.is_field_marked(y, x - 1) {
+                neighbours.push((y, x - 1, n));
+            }
+        };
+        if let Some(n) = right_o {
+            if !self.is_field_marked(y, x + 1) {
+                neighbours.push((y, x + 1, n));
+            }
+        };
+        if let Some(n) = top_o {
+            if !self.is_field_marked(y - 1, x) {
+                neighbours.push((y - 1, x, n));
+            }
+        };
+        if let Some(n) = bottom_o {
+            if !self.is_field_marked(y + 1, x) {
+                neighbours.push((y + 1, x, n));
+            }
+        };
+
+        neighbours
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct Field {
+    pub coordinate: (usize, usize),
+    pub path_length: u32,
+    pub cost: u32,
+}
+
+impl Field {
+    pub fn new(coordinate: (usize, usize), cost: u32, path_length: u32) -> Field {
+        Field {
+            coordinate,
+            cost,
+            path_length,
+        }
+    }
+}
+
+impl PartialOrd<Self> for Field {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.cost > other.cost {
+            return Some(Ordering::Less);
+        }
+
+        if self.cost == other.cost {
+            return Some(Ordering::Equal);
+        }
+
+        return Some(Ordering::Greater);
+    }
+}
+
+impl Ord for Field {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.cost > other.cost {
+            return Ordering::Less;
+        }
+
+        if self.cost == other.cost {
+            return Ordering::Equal;
+        }
+
+        return Ordering::Greater;
+    }
+}
---
+++ src/bin/12/main.rs
@@ -0,0 +1,163 @@
+#![feature(test)]
+
+pub mod grid;
+
+use crate::grid::{manhattan_distance, Field, Grid};
+use std::cmp::min;
+use std::collections::BinaryHeap;
+use std::iter::zip;
+
+type Solution = u32;
+type ParseOutput = (Grid, (usize, usize), (usize, usize));
+
+const MAIN_INPUT: &str = include_str!("main_input");
+
+pub fn parse(file: &str) -> ParseOutput {
+    let mut grid = Grid::new((file.lines().count(), file.lines().next().unwrap().len()));
+    let mut start = (0, 0);
+    let mut end = (0, 0);
+
+    for (y, l) in file.lines().enumerate() {
+        for (x, character) in l.bytes().enumerate() {
+            grid.set_field_height(
+                y,
+                x,
+                match character {
+                    83 => {
+                        start = (y, x);
+                        0
+                    }
+                    69 => {
+                        end = (y, x);
+                        25
+                    }
+                    c => c as u32 - 97,
+                },
+            );
+        }
+    }
+
+    (grid, start, end)
+}
+
+fn part_1(parse_output: &ParseOutput) -> Solution {
+    calc_fastest_path(parse_output.clone())
+}
+
+fn part_2(parse_output: &ParseOutput) -> Solution {
+    let (grid, _, end_coords) = parse_output.clone();
+    let size = grid.get_size();
+    let mut fastest = u32::MAX;
+
+    for y in 0..size.0 {
+        for x in 0..size.1 {
+            if grid.get_field_height(y, x) == 0 {
+                let path_len = calc_fastest_path((grid.clone(), (y, x), end_coords));
+                if path_len == 0 {
+                    continue;
+                }
+                fastest = min(fastest, path_len);
+            }
+        }
+    }
+    fastest
+}
+
+fn calc_fastest_path(mut out: ParseOutput) -> Solution {
+    let (mut grid, start_coord, end_coords) = out;
+    let mut open_fields = BinaryHeap::<Field>::new();
+    let start = Field::new(start_coord, 0, 0);
+    grid.mark_field(start_coord.0, start_coord.1);
+
+    open_fields.push(start);
+
+    'outer: while !open_fields.is_empty() {
+        let current_field = open_fields.pop().unwrap();
+        let neighbours =
+            grid.get_unmarked_neighbours(current_field.coordinate.0, current_field.coordinate.1);
+        for (y, x, height) in neighbours {
+            let field_cost = calc_cost(
+                &(
+                    current_field.coordinate,
+                    grid.get_field_height(current_field.coordinate.0, current_field.coordinate.1),
+                ),
+                (y, x, height),
+                &end_coords,
+            );
+            if field_cost < u32::MAX {
+                if (y, x) == end_coords {
+                    return current_field.path_length + 1;
+                }
+                grid.mark_field(y, x);
+                open_fields.push(Field::new(
+                    (y, x),
+                    current_field.cost + field_cost,
+                    current_field.path_length + 1,
+                ));
+            }
+        }
+    }
+
+    0
+}
+
+fn calc_cost(
+    field: &((usize, usize), u32),
+    neighbour: (usize, usize, u32),
+    end: &(usize, usize),
+) -> u32 {
+    if (neighbour.2 as i32 - field.1 as i32) > 1 {
+        return u32::MAX;
+    }
+
+    manhattan_distance(field.0, *end)
+}
+
+fn main() {
+    let parse_output = parse(MAIN_INPUT);
+    println!("Solution to part 1 is {}", part_1(&parse_output));
+    println!("Solution to part 2 is {}", part_2(&parse_output));
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+    use super::*;
+    use test::{black_box, Bencher};
+    const TEST_INPUT: &str = include_str!("test_input");
+
+    #[test]
+    pub fn test_part_1() {
+        let parse_output = parse(TEST_INPUT);
+        assert_eq!(part_1(&parse_output), 31);
+    }
+
+    #[test]
+    pub fn test_part_2() {
+        let parse_output = parse(TEST_INPUT);
+        assert_eq!(part_2(&parse_output), 29);
+    }
+
+    #[bench]
+    fn bench_parse(b: &mut Bencher) {
+        b.iter(|| {
+            let _ = parse(MAIN_INPUT);
+        });
+    }
+
+    #[bench]
+    fn bench_part_1(b: &mut Bencher) {
+        let parse_output = parse(MAIN_INPUT);
+        b.iter(move || {
+            assert_eq!(part_1(black_box(&parse_output)), 330);
+        });
+    }
+
+    #[bench]
+    fn bench_part_2(b: &mut Bencher) {
+        let parse_output = parse(MAIN_INPUT);
+        b.iter(|| {
+            assert_eq!(part_2(black_box(&parse_output)), 321);
+        });
+    }
+}
